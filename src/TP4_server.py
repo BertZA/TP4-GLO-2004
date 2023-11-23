4cb9e9d03b160526ec95ca3ef87b6ca231ef4b5f
Bertrand <beawe@ulaval.ca>
2023-11-23 00:53:37 -0500

    Update TP4_server.py
    Avancement avec le serveur !

--- src/TP4_server.py
+++ src/TP4_server.py
@@ -17,6 +17,7 @@ import sys
 import glosocket
 import gloutils
 
+import re
 
 class Server:
     """Serveur mail @glo2000.ca."""
@@ -37,15 +38,15 @@ class Server:
         self._server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
         try:
-            self._server_socket.bind("localhost", 1400)
+            self._server_socket.bind("localhost", gloutils.APP_PORT)
             self._server_socket.listen()
 
         except socket.error as e:
             sys.exit(-1)
 
-        self._client_socs : list = []
-        self._logged_users : dict = {}
-
+        self._client_socs : list[socket.socket] = []
+        self._logged_users : dict[socket.socket : str] = {}
+        self._client_accounts: dict[str : str] = {}
 
     def cleanup(self) -> None:
         """Ferme toutes les connexions résiduelles."""
@@ -55,11 +56,16 @@ class Server:
 
     def _accept_client(self) -> None:
         """Accepte un nouveau client."""
+        client, x = self._server_socket.accept()
+        self._client_socs.append(client)
+        #envoyer un message de bienvenue ici
+
 
     def _remove_client(self, client_soc: socket.socket) -> None:
         """Retire le client des structures de données et ferme sa connexion."""
         self._client_socs.remove(client_soc)
         self._logged_users.pop(client_soc)
+        client_soc.close()
 
     def _create_account(self, client_soc: socket.socket,
                         payload: gloutils.AuthPayload
@@ -71,7 +77,33 @@ class Server:
         associe le socket au nouvel l'utilisateur et retourne un succès,
         sinon retourne un message d'erreur.
         """
-        return gloutils.GloMessage()
+        header : int = gloutils.Headers.ERROR
+        msg : str = ""
+        
+        username : str = payload['username'].capitalize()
+        password : str = payload['password']
+
+        if re.search(r"\w", username) is not None:
+            if not username in self._client_accounts:
+            #verifier nom pris ou pas
+                if len(password) >= 10:
+                    reg = re.compile(r'^(?=.*[0-9])(?=.*[A-Z])(?=.*[a-z]).+$')
+                    if reg.match(password):
+                        #créer le dossier ici
+                        pass_hashed = hashlib.sha3_512()
+                        pass_hashed.update(password)
+                        password = pass_hashed.hexdigest()
+                        #écrire dans le fichier dans le dossier ici
+                        return gloutils.GloMessage(gloutils.Headers.OK)
+                    else:
+                        msg = "Votre mot de passe n'est pas assez fort"
+                else:
+                    msg = "Votre mot de passe n'est assez long"
+            else:
+                msg = "Ce nom d'utilisateur existe déjà"
+        else:
+            msg = "Votre nom d'utilisateur ne doit contenir que de caractères alphanuméques"
+        return gloutils.GloMessage(header, msg)
 
     def _login(self, client_soc: socket.socket, payload: gloutils.AuthPayload
                ) -> gloutils.GloMessage:
@@ -81,10 +113,29 @@ class Server:
         Si les identifiants sont valides, associe le socket à l'utilisateur et
         retourne un succès, sinon retourne un message d'erreur.
         """
-        return gloutils.GloMessage()
+        message: gloutils.GloMessage = gloutils.GloMessage(
+            gloutils.Headers.ERROR,
+            "Désolé, nous ne sommes pas parvenus à vous connecter"
+        )
+        
+        username: str = payload['username'].capitalize()
+        pass_hashed = hashlib.sha3_512()
+        pass_hashed.update(payload['password'])
+        password :str = pass_hashed.hexdigest();
+
+        if username in self._client_accounts :
+            if self._client_accounts[username] == password:
+                self._logged_users[client_soc] = username
+                message = gloutils.GloMessage(
+                    gloutils.Headers.OK
+                )
+                
+        return message
 
     def _logout(self, client_soc: socket.socket) -> None:
         """Déconnecte un utilisateur."""
+        self._logged_users.pop(client_soc)
+
 
     def _get_email_list(self, client_soc: socket.socket
                         ) -> gloutils.GloMessage:
